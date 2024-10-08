755bd20f90e184f8cb6b43529d8a3c2b4ad6f733
Alex Antonyuk <oantoniuk-ecw@cricut.com>
2024-10-08 16:57:05 +0300

    Initial Commit

---
+++ SharedStateSandbox/SharedStateSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedStateSandboxApp.swift
+//  SharedStateSandbox
+//
+//  Created by oantoniuk on 08.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SharedStateSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
